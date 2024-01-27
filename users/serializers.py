a7f8ea8767687dfbcc8ecb35aa2b36e7b72152b6
Brajenko <goshabrajenko@gmail.com>
2024-01-27 15:15:18 +0300

    fixed problems with password changing

--- users/serializers.py
+++ users/serializers.py
@@ -9,12 +9,18 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'first_name', 'last_name', 'email', 'birthday', 'password']
     
-    def create(self, data):
-        password = data.pop('password', None)
-        user = self.Meta.model(**data)
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = self.Meta.model(**validated_data)
         if user.birthday >= datetime.date.today():
             raise ValidationError('The date cannot be in the future.')
         if password is not None:
             user.set_password(password)
         user.save()
         return user
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password is not None:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
